38a8e7a7f2bcbc06731da03f38ccee075c9d1c78
ElectronicLED <noureldinaymanshalaby@gmail.com>
2025-07-13 22:53:47 +0300

    changed training velocity direction

--- euflex_train.py
+++ euflex_train.py
@@ -138,8 +138,8 @@ def get_cfgs():
     }
     command_cfg = {
         "num_commands": 3,
-        "lin_vel_x_range": [0.2, 0.2],
-        "lin_vel_y_range": [0, 0],
+        "lin_vel_x_range": [0, 0],
+        "lin_vel_y_range": [0.5, 0.5],
         "ang_vel_range": [0, 0],
     }
 
