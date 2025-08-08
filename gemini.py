6041a0da475a1ed0ea0097d44c30372e912849b9
ElectronicLED <noureldinaymanshalaby@gmail.com>
2025-08-08 11:10:26 +0300

    used the chat feature with pyttsx3 to make livelike conversations

--- gemini.py
+++ gemini.py
@@ -1,9 +1,21 @@
 from google import genai
 import os 
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))  # Set your API key here
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))  # Set your API key here. I am not showing mine
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="what do you know about humanoids"
-)
-print(response.text)
+# response = client.models.generate_content(
+#     model="gemini-2.5-flash", contents="what do you know about humanoids"
+# )
+# print(response.text)
+
+
+dialog = client.chats.create(model="gemini-2.5-flash")
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        print("Exiting chat.")
+        break
+    response = dialog.send_message(user_input)
+    print("NUBI: " + response.text)
+    
---
+++ gemini_speak.py
@@ -0,0 +1,34 @@
+from google import genai
+import pyttsx3
+import os 
+
+engine = pyttsx3.init("espeak")  # Use espeak-ng as the TTS engine
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))  # Set your API key here i am not showin mine
+
+# response = client.models.generate_content(
+#     model="gemini-2.5-flash", contents=input("How can I help you today? " + "\n")
+# ).text
+
+# for word in response.splitlines():
+#     print(word)
+#     engine.say(word)
+#     engine.runAndWait()
+
+
+chat = client.chats.create(model="gemini-2.5-flash")
+
+# initial prompt to let it know what it is
+response = chat.send_message('''Your name is NUBI. 
+    You are Nour ElDeens Shalaby's graduation project: humanoid bipedal robot. 
+    NUBI stands for Next Unit Bionic Intelligence''').text
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        print("Exiting chat.")
+        break
+    response = chat.send_message(user_input).text
+    response = response.replace("*", "")  # Remove asterisks if present
+    print("NUBI: " + response)
+    engine.say(response)
+    engine.runAndWait()
