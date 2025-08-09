c2091a7f27c68247205a05018c8f21318827c4a0
ElectronicLED <noureldinaymanshalaby@gmail.com>
2025-08-09 14:32:56 +0300

    rewrote tts_corgi entirely and it works now

--- tts_corgi.py
+++ tts_corgi.py
@@ -1,13 +1,34 @@
+# Installation: pip install TTS
 from TTS.api import TTS
-import torch
+from playsound import playsound
 
 
-# Get device
-device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# TTS with on the fly voice conversion
-api = TTS("tts_models/en/sam/tacotron-DDC").to(device)
-api.tts_to_file(
-    text="hello world",
-    file_path="output.wav"
-)
+###################### Testing all sounds ##################################################3
+# model_names = ["tts_models/en/ljspeech/tacotron2-DDC",
+#                #"tts_models/en/ek1/tacotron2",
+#                "tts_models/en/ljspeech/glow-tts",
+#                #"tts_models/en/ljspeech/tacotron2-DCA",
+#                #"tts_models/en/ljspeech/speedy-speech-wn",
+#                #"tts_models/en/sam/tacotron-DDC",
+#                #"tts_models/en/vctk/sc-glow-tts",
+#                ]
+
+# for i in range(len(model_names)):
+#     print(f"\n\n\nModel {i}: {model_names[i]}\n\n\n\n")
+
+#     tts = TTS(model_name=model_names[i], progress_bar=True, gpu=True)
+#     tts.tts_to_file(text="Hello My Name Is NUBI", file_path=f"{i}.wav")
+
+##################################################################################################
+
+
+# file_path = "sounds/corgi_output.wav"  # Specify the output file path
+# tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC", progress_bar=True, gpu=False)
+# tts.tts_to_file(text="Hello My Name Is NUBI. I am 3 months old", file_path=file_path)
+# playsound(file_path)
+
+def say(line="Hi", voice_name="Fenrir", output_file_name="output_file"):
+    tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC", progress_bar=True, gpu=False)
+    tts.tts_to_file(text=line, file_path=output_file_name+".wav")
+    playsound(output_file_name+".wav")
