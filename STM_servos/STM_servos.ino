2ca022d766d7dc233d320cf9a4cf01cd16193709
ElectronicLED <noureldinaymanshalaby@gmail.com>
2025-11-17 15:35:11 +0200

    modified the buad rate to 115200

--- STM_servos/STM_servos.ino
+++ STM_servos/STM_servos.ino
@@ -4,8 +4,11 @@ int n=18; //motor ID - verify your ID !!!!
 
 void setup()  
 {
+  
+  Serial.begin(9600); //and it works with sirial monitor !!
+  pinMode(LED_BUILTIN, OUTPUT);
   delay(2000);  //a delay to have time for serial monitor opening
-  Herkulex.beginSerial1(57600,PA9,PA10); //open serial with rx=PB7 and tx=PB6 
+  Herkulex.begin(115200,PA9,PA10); //open serial with rx=PB7 and tx=PB6 
   Herkulex.reboot(n); //reboot first motor
   delay(500); 
   Herkulex.initialize(); //initialize motors
@@ -13,9 +16,12 @@ void setup()
 }
 
 void loop(){
+  Serial.println("-100");
+  digitalWrite(LED_BUILTIN, HIGH); 
   Herkulex.moveOneAngle(n, -100, 1000, LED_BLUE); //move motor with 300 speed  
   delay(1200);
- 
+  Serial.println("100");
+  digitalWrite(LED_BUILTIN, LOW); 
   Herkulex.moveOneAngle(n, 100, 1000, LED_BLUE); //move motor with 300 speed  
   delay(1200);
   
