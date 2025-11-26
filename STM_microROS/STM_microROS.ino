6f6b69883e37edd7ff83a75f648a567b32b464cf
ElectronicLED <noureldinaymanshalaby@gmail.com>
2025-11-26 15:46:08 +0200

    added legs functions prototype

--- STM_microROS/STM_microROS.ino
+++ STM_microROS/STM_microROS.ino
@@ -9,9 +9,16 @@
 #include <rclc/executor.h>
 
 #include <std_msgs/msg/int16.h>
+#include <std_msgs/msg/int16_multi_array.h>
+
 
 rcl_subscription_t subscriber;
-std_msgs__msg__Int16 msg;
+rcl_publisher_t publisher;
+//std_msgs__msg__Int16 msg;
+std_msgs__msg__Int16MultiArray legs_command;
+std_msgs__msg__Int16MultiArray legs_feedback;
+// int test[12];
+
 rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
@@ -23,6 +30,10 @@ rcl_timer_t timer;
 
 int n=18;
 
+const uint leg_motor_indecies[12] = {1,2,3,4,5,6,7,8,9,10,11,12};
+
+
+
 void error_loop(){
   while(1){
     digitalWrite(PB6, !digitalRead(PB6));
@@ -30,22 +41,33 @@ void error_loop(){
   }
 }
 
-void subscription_callback(const void * msgin)
-{
-  const std_msgs__msg__Int16 * msg = (const std_msgs__msg__Int16 *)msgin;
-  //The print is not working
-  Serial.println(msg->data);
-  if (msg->data == 90)
-  {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(250);
-    digitalWrite(LED_BUILTIN, LOW);
+void legs_cmd_callback(const void * msgin){
+  const std_msgs__msg__Int16MultiArray * msg = (const std_msgs__msg__Int16MultiArray *)msgin;
+  
+  for(int i = 0; i<12;i++){
+    Herkulex.moveOneAngle(leg_motor_indecies[i], msg->data.data[i], 1000, LED_BLUE);
+    // test[0]=msg->data.data[0];
+    // test[1]=msg->data.
+    //Serial.println(test);
   }
-  Herkulex.moveOneAngle(n, msg->data, 1000, LED_BLUE); //move motor  
-  delay(1000);
-  Serial.println("Servo angle:");
-  Serial.println(Herkulex.getAngle(n));
 }
+
+// void subscription_callback(const void * msgin)
+// {
+//   const std_msgs__msg__Int16 * msg = (const std_msgs__msg__Int16 *)msgin;
+//   //The print is not working
+//   Serial.println(msg->data);
+//   if (msg->data == 90)
+//   {
+//     digitalWrite(LED_BUILTIN, HIGH);
+//     delay(250);
+//     digitalWrite(LED_BUILTIN, LOW);
+//   }
+//   Herkulex.moveOneAngle(n, msg->data, 1000, LED_BLUE); //move motor  
+//   delay(1000);
+//   Serial.println("Servo angle:");
+//   Serial.println(Herkulex.getAngle(n));
+// }
 
 void setup() {
   pinMode(LED_BUILTIN,OUTPUT);
@@ -65,15 +87,23 @@ void setup() {
   RCCHECK(rclc_subscription_init_default(
     &subscriber,
     &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16),
-    "NUBI_STM_TOPIC"));
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16MultiArray),
+    "legs_command"));
+
+  // create publisher
+  RCCHECK(rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16MultiArray),
+    "legs_feedback"));
 
   // create executor
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));  
-  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+  //RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &legs_command, &legs_cmd_callback, ON_NEW_DATA));
 
   //Servo initialization
-  Serial.begin(115200); //and it works with sirial monitor !!
+  //Serial.begin(115200); //and it works with sirial monitor !!
   delay(2000);  //a delay to have time for serial monitor opening
   Herkulex.begin(115200,PA9,PA10); //open serial with rx=PB7 and tx=PB6 
   Herkulex.reboot(n); //reboot first motor
