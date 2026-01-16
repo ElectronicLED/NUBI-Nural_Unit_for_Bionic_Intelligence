2c5fa75d4e8500ff92c82fb4760230c749180b53
ElectronicLED <noureldinaymanshalaby@gmail.com>
2026-01-16 17:53:17 +0200

    added robotic floss

--- robot.py
+++ robot.py
@@ -159,20 +159,79 @@ class robot_actions_controller():
         time.sleep(2)
         self.default_stance()
 
+    def floss(self, i=11):
+        self.default_stance()
+        time.sleep(0.5)
+        upper_cmd =  Int16MultiArray()
+        lower_cmd = Int16MultiArray()
+
+        while i >0:
+            upper_cmd.data = self.data["floss0"]["upper_body"]
+            self.publisher_upperbody.publish(upper_cmd)
+            time.sleep(0.1)
+            lower_cmd.data = self.data["floss0"]["lower_body"]
+            self.publisher_legs.publish(lower_cmd)
+            time.sleep(1.2)
+
+            upper_cmd.data = self.data["floss1"]["upper_body"]
+            self.publisher_upperbody.publish(upper_cmd)
+            time.sleep(0.1)
+            lower_cmd.data = self.data["floss1"]["lower_body"]
+            self.publisher_legs.publish(lower_cmd)
+            time.sleep(1.2)
+
+            upper_cmd.data = self.data["floss2"]["upper_body"]
+            self.publisher_upperbody.publish(upper_cmd)
+            time.sleep(0.2)
+            lower_cmd.data = self.data["floss2"]["lower_body"]
+            self.publisher_legs.publish(lower_cmd)
+            time.sleep(1.2)
+
+            upper_cmd.data = self.data["floss3"]["upper_body"]
+            self.publisher_upperbody.publish(upper_cmd)
+            time.sleep(0.2)
+            lower_cmd.data = self.data["floss3"]["lower_body"]
+            self.publisher_legs.publish(lower_cmd)
+            time.sleep(1.2)
+
+            upper_cmd.data = self.data["floss4"]["upper_body"]
+            self.publisher_upperbody.publish(upper_cmd)
+            time.sleep(0.2)
+            lower_cmd.data = self.data["floss4"]["lower_body"]
+            self.publisher_legs.publish(lower_cmd)
+            time.sleep(1.2)
+
+            upper_cmd.data = self.data["floss3"]["upper_body"]
+            self.publisher_upperbody.publish(upper_cmd)
+            time.sleep(0.2)
+            lower_cmd.data = self.data["floss3"]["lower_body"]
+            self.publisher_legs.publish(lower_cmd)
+            time.sleep(1.2)
+
+            i-=1
+        self.default_stance()
+
+
+
+
+
 
 if __name__ == "__main__":
     rclpy.init()
     robot_actions_controller = robot_actions_controller()
+
     robot_actions_controller.default_stance()
     time.sleep(1)
-    robot_actions_controller.wave(2)
-    robot_actions_controller.default_stance()
-    time.sleep(1)
-    robot_actions_controller.fight_stance()
-    time.sleep(1)
-    robot_actions_controller.jab()
-    time.sleep(1)
-    robot_actions_controller.cross()
-    time.sleep(1)
-    robot_actions_controller.default_stance()
-    time.sleep(2)
+    robot_actions_controller.floss(2)
+    # time.sleep(1)
+    # robot_actions_controller.wave(2)
+    # robot_actions_controller.default_stance()
+    # time.sleep(1)
+    # robot_actions_controller.fight_stance()
+    # time.sleep(1)
+    # robot_actions_controller.jab()
+    # time.sleep(1)
+    # robot_actions_controller.cross()
+    # time.sleep(1)
+    # robot_actions_controller.default_stance()
+    # time.sleep(2)
